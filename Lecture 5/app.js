059f9c43d70c4cd5faae31d1c7f752d20f3d1934
Angel Amaya <angelamaya880@gmail.com>
2020-08-10 22:29:11 -0500

    Ejercicio 5

---
+++ Lecture 5/app.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+angular.module('myFirstApp',[])
+
+.controller('MyFirstController',function ($scope){
+  $scope.name = "Angel";
+  $scope.sayHello = function() {
+    return "Hello Coursera!";
+  };
+});
+
+})();
